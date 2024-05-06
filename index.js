a2b9778be5544e28ae322516060d9600724f1d57
Jaslan Daulenov <jaslan.daulenov@gmail.com>
2024-05-06 11:39:31 +0300

    refactor: add headers Origin-Trial

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const express = require('express');
+
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+// Промежуточное ПО для установки заголовка Origin-Trial
 app.use((req, res, next) => {
-    console.log(req.headers);
+    // Устанавливаем заголовок Origin-Trial для всех запросов
+    req.headers['Origin-Trial'] = 'true';
     next();
+    console.log(req.headers);
 });
 
 app.get('/', (req, res) => {
     res.send('Webview test');
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log(`Server has been started on port ${port}`);
 });
